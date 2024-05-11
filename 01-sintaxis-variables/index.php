63ef6706bd5577562fa6faa7caaccd8e3e74160d
unknown <aandresfelipeh8013@gmail.com>
2024-05-11 00:41:52 -0500

    feat: ejercicios condicionales

--- 01-sintaxis-variables/index.php
+++ 01-sintaxis-variables/index.php
@@ -1,3 +1,18 @@
 <?php
-echo 'ejecutando sintaxis variables'
+echo "<h2>Ejercicio</h2>";
+$nombre1 = "Pedro";
+$nombre2 = "Ana";
+$saludo = "hola";
+echo "$saludo $nombre1 y $nombre2 ¿como están?";
+echo "<br> <br>";
+
+echo "<h2>Ejercicio: truco de magia matemática </h2>";
+$mi_numero = 9;
+$respuesta = $mi_numero;
+$respuesta += "2";
+$respuesta *= "2";
+$respuesta -= "2";
+$respuesta /= "2";
+$mi_numero -= $respuesta;
+echo "$mi_numero";
 ?>
--- 02-condicionales/index.php
+++ 02-condicionales/index.php
@@ -1,3 +1,70 @@
 <?php
-echo 'ejecutando sintaxis variables'
+echo "<h2>Ejercicio 1</h2> <br>";
+$cantidadDeArticulos = 10 ;
+if($cantidadDeArticulos <= 12)
+{echo "Caja Rápida <br> <br>";}
+else {echo "CAJA NORMAL <br> <br>";}
+
+$edadPaVotar = 10 ;
+if($edadPaVotar >= 18)
+{echo "SÍ";}
+else {echo "NO";}
+
+echo "<h2>Ejercicio 2 <br></h2>";
+$edadDiscoteca = 11;
+$nombreDiscoteca = "mario";
+
+if($edadDiscoteca >= 18)
+{echo "PUEDE INGRESAR A LA DISCOTECA";}
+else if($edadDiscoteca <= 17)
+{echo "no puede ingresar a la discoteca";}
+else if($nombreDiscoteca === "mario" or "carlos")
+{echo "Puede Ingresar";}
+
+echo "<h2>Ejercicio 3 <br></h2>";
+$estatura = 190;
+$velocidad = 29;
+$edadParticipante = 40; 
+
+$asignaciónParticipante = null;
+
+
+if($estatura >= 170 && $velocidad >= 27)
+{echo "INGRESO ADMITIDO <br>";
+if($edadParticipante >= 18)
+{echo "DIVISIONES MAYORES";}
+else {echo "divisiones menores";}
+}
+else {echo "ingreso negado";}
+
+
+echo "<h2>Ejercicio 4 <br></h2>";
+
+$colorAire = "perejil";
+$calidadAire = "la calidad del aire es"; 
+
+switch ($colorAire)
+{
+case "verde":
+    echo "$calidadAire buena";
+break;
+case "amarillo":
+    echo"$calidadAire moderada";
+break;
+case "naranja":
+    echo "$calidadAire saludable para grupos sensibles";
+break;
+case "rojo":
+    echo"$calidadAire saludable";
+break;
+case "purpura":
+    echo"$calidadAire muy poco saludable";
+break;
+case "marron":
+    echo"$calidadAire peligrosa";
+break;
+default:
+    echo "color no válido";
+}
+/* else {echo "color no válido";} */
 ?>
